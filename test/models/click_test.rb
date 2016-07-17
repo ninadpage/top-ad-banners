bf2542458166f6c5669aead1bdfd7c5b118450d6
ninadpage <ninad1902@gmail.com>
2016-07-17 18:24:28 +0200

    Added basic unit tests for Models, and generated test data

--- test/models/click_test.rb
+++ test/models/click_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ClickTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should save two clicks for same banner and campaign' do
+    assert_not_nil Click.create banner_id: 3, campaign_id: 1
+    assert_not_nil Click.create banner_id: 3, campaign_id: 1
+  end
 end
--- test/models/conversion_test.rb
+++ test/models/conversion_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ConversionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Should not save conversion without valid click_id' do
+    assert_raise { Conversion.create click: 1000000, revenue: 1.0 }
+  end
+
+  test 'Should save conversion' do
+    assert_not_nil Conversion.create click: Click.find(1), revenue: 1.0
+  end
 end
